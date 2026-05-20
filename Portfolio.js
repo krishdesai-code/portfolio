a7518d54d1aa270e9ce74e230c5feb7145567bba
Krish Desai <krishdesai183@gmail.com>
2026-05-20 10:26:35 +0530

    New Features Added!!

--- Portfolio.js
+++ Portfolio.js
@@ -14,3 +14,47 @@ openMenu.addEventListener("click", () => {
         isOpen = false;
     }
 });
+
+let btn = document.getElementById("btnT");
+
+btn.addEventListener('click',()=>{
+    let body = document.body;
+    let menu = document.getElementById("openMenu");
+    let curr = "light";
+
+    if (curr == "light") {
+        body.classList.toggle("dark");
+        menu.classList.toggle("darkImg");
+        curr = "dark";
+    }
+
+    else
+    {
+        body.classList.toggle("light");
+        curr = "light";
+    }
+    
+});
+
+
+let Sbtns = document.querySelectorAll(".Sbtn");
+
+Sbtns.forEach((Sbtn) => {
+
+Sbtn.addEventListener('click',()=>{
+
+    let para = Sbtn.nextElementSibling;
+    
+    para.classList.toggle("show");
+    para.classList.toggle("remove");
+
+    if(para.classList.contains("show")){
+        Sbtn.textContent = "Hide";
+    }
+    else{
+        Sbtn.textContent = "See More";
+    }
+
+});
+
+});
